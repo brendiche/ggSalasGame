3c4e9ccd4f85c2f101b77b2545c97526db53a089
bgouin <bgouin@pubstack.io>
2023-03-04 20:07:55 +0100

    comment main code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import './assets/main.css';
 
-const img = document.createElement('div');
-img.className = 'img'
-const txt = document.createElement('h1');
-txt.innerText = 'Game in Progress';
-txt.className = 'txt';
+// const img = document.createElement('div');
+// img.className = 'img'
+// const txt = document.createElement('h1');
+// txt.innerText = 'Game in Progress';
+// txt.className = 'txt';
 
 
-document.body.appendChild(img);
-document.body.appendChild(txt);
+// document.body.appendChild(img);
+// document.body.appendChild(txt);
