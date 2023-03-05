076ea881f2a53c167a7b88bd953262eef98b2aa7
bgouin <bgouin@pubstack.io>
2023-03-05 23:46:20 +0100

    chore: improve architecture

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './assets/main.css';
 import './assets/maps/maps.css';
-import { Level } from './level';
+import { Level, MapConfig } from './level';
 import { Screen } from './screen';
 
 const screen = new Screen();
@@ -8,40 +8,23 @@ const screen = new Screen();
 /**
  * This will going in a senario class
  */
-const firstLevel = new Level()
-firstLevel.map('room');
+const {heightScreen, widthScreen} = screen.getSize();
+const getWidth = (w:number) => ((180/359)*w - (104172/359)); // TODO 2023-03-05 put this in helper file
+const getHeight = (h:number) => ((1/2)*h - (192)); // TODO 2023-03-05 put this in helper file
+const heightMap = 352; // TODO 2023-03-05 put this in config file
+const widthMap = 577; // TODO 2023-03-05 put this in config file
+const firstLevelConfig: MapConfig = {
+  box:{
+    height: heightMap,
+    width: widthMap,
+    left: Math.trunc(getWidth(widthScreen)),
+    top: Math.trunc(getHeight(heightScreen)),
+  }
+}
+const firstLevel = new Level('room', firstLevelConfig)
 /**
  * 
- */
+*/
 
 document.body.appendChild(firstLevel.getElement());
-
-/** test zone */
-const getWidth = (w:number) => ((180/359)*w - (104172/359));
-const getHeight = (h:number) => ((1/2)*h - (192));
-
-const heightMap = 352;
-const widthMap = 577;
-
-const {heightScreen, widthScreen} = screen.getSize();
-
-const test = document.createElement('div');
-test.id = 'test';
-test.style.backgroundColor = new URLSearchParams(window.location.search).get('bgc') ??'none';
-test.style.width = `${widthMap}px`;
-test.style.height = `${heightMap}px`;
-test.style.position = 'absolute';
-test.style.top = `${Math.trunc(getHeight(heightScreen))}px`;
-test.style.left = `${Math.trunc(getWidth(widthScreen))}px`;
-test.style.opacity = '50%';
-
-screen.onResize(() => {
-  const rzEl = document.getElementById('test');
-  rzEl.style.top = `${Math.trunc(getHeight(screen.getSize().heightScreen))}px`;
-  test.style.left = `${Math.trunc(getWidth(screen.getSize().widthScreen))}px`;
-})
-
-
-document.body.append(test);
-
-/** end test zone */
+firstLevel.debug();
--- src/level.ts
+++ src/level.ts
@@ -1,10 +1,21 @@
 const MAP_CONTAINER_CLASS = 'map-container';
 
+export interface MapConfig {
+  box: {
+    width:number,
+      height: number,
+      top: number,
+      left: number,
+  }
+}
+
 export class Level{
   private _map: string;
+  private mapConfig: MapConfig;
 
-  map(className: string){
-    this._map = className;
+  constructor(mapName: string, config: MapConfig){
+    this._map = mapName;
+    this.mapConfig = config;
   }
 
   getElement(): HTMLDivElement{
@@ -14,5 +25,26 @@ export class Level{
     map.className = this._map;
     el.appendChild(map);
     return el;
+  }
+
+  debug(){
+    const test = document.createElement('div');
+    test.id = 'test';
+    test.style.backgroundColor = new URLSearchParams(window.location.search).get('bgc') ??'none';
+    test.style.position = 'absolute';
+    test.style.opacity = '50%';
+    test.style.width = `${this.mapConfig.box.width}px`;
+    test.style.height = `${this.mapConfig.box.height}px`;
+    test.style.top = `${this.mapConfig.box.top}px`;
+    test.style.left = `${this.mapConfig.box.left}px`;
+
+    // screen.onResize(() => {
+    //   const rzEl = document.getElementById('test');
+    //   rzEl.style.top = `${Math.trunc(getHeight(screen.getSize().heightScreen))}px`;
+    //   test.style.left = `${Math.trunc(getWidth(screen.getSize().widthScreen))}px`;
+    // })
+
+
+    document.body.append(test);
   }
 }
