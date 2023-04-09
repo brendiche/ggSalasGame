1f6c52315797556ee4ec3964d2c97eb1250a6034
brendiche <brendan.gouin92@gmail.com>
2023-04-09 14:39:08 +0200

    wit: env var test

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,12 +1,15 @@
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import { NetlifyAPI } from 'netlify';
 
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
+  const client: any = new NetlifyAPI(process.env.sites, {});
+  const sites = await client.listSites();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    body: JSON.stringify({ message: 'Hello World', sites: sites }),
   };
 };
 
