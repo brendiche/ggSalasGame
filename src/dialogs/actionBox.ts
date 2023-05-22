af601e0bbdadd993a2cadbb26074ca97ea69fb4a
brendiche <brendan.gouin92@gmail.com>
2023-05-22 22:26:27 +0200

    feat: add action box

---
+++ src/dialogs/actionBox.ts
@@ -0,0 +1,40 @@
+import { boxItem } from '../types';
+
+export class ActionBox {
+  private text: string;
+  private line: HTMLElement;
+  private txtBox: HTMLElement;
+
+  constructor(text: string) {
+    this.text = text;
+  }
+
+  display() {
+    this.txtBox.style.display = 'block';
+  }
+
+  hide() {
+    this.txtBox.style.display = 'none';
+  }
+
+  createBox(box: boxItem): void {
+    if (!this.txtBox) {
+      this.txtBox = document.createElement('div');
+      this.txtBox.className = 'text-action';
+      this.txtBox.id = 'txtBox';
+      this.txtBox.style.display = 'none';
+      this.txtBox.style.top = `${box.top}px`;
+      this.txtBox.style.left = `${box.left}px`;
+      this.txtBox.style.width = `${box.width}px`;
+      this.txtBox.style.height = `${box.height}px`;
+    }
+    document.body.appendChild(this.txtBox);
+  }
+
+  writeText(): void {
+    this.line = document.createElement('div');
+    this.line.className = 'line-1';
+    this.line.append(document.createTextNode(this.text));
+    this.txtBox.appendChild(this.line);
+  }
+}
